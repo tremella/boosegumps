b23160b90f70f8f49b7655212a473f53d1541c49
tremella <jeshbrand@gmail.com>
2023-10-28 19:47:09 +0100

    renamed hashes

--- storyParts.js
+++ storyParts.js
@@ -4,16 +4,16 @@ const storyParts = {
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option1a",
+              nextPart: "enter_together",
               text: "Enter mansion",
           },
           B: {
-              nextPart: "option1b",
+              nextPart: "go_home",
               text: "Go home",
           }
       }
   },
-  option1a: {
+  enter_together: {
       text: `You enter the mansion together and look around. Library or hum?.`,
       image: "assets/cover_house.jpg",
       options: {
@@ -22,21 +22,21 @@ const storyParts = {
               text: "Enter library",
           },
           B: {
-              nextPart: "option2b",
+              nextPart: "follow_hum",
               text: "Follow the mysterious hum",
           }
       },
   },
-  option1b: {
+  go_home: {
       text: `You decide to go home, but Alex runs into the mansion. You follow him.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option1a",
+              nextPart: "enter_together",
               text: "Follow Alex (to make sure he's safe)",
           },
           B: {
-              nextPart: "option1a",
+              nextPart: "enter_together",
               text: "Follow Alex (to yell at him)",
           }
       },
@@ -46,30 +46,30 @@ const storyParts = {
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option3a",
+              nextPart: "read_contents",
               text: "Read contents",
           },
           B: {
-              nextPart: "option3b",
+              nextPart: "examine_binding",
               text: "Examine binding",
           }
       }
   },
-  option2b: {
+  follow_hum: {
       text: `You follow the hum towards a basement. You also notice a shadow in a nearby room. Basement or shadow?`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option4a",
+              nextPart: "inspect_basement",
               text: "Basement",
           },
           B: {
-              nextPart: "option4b",
+              nextPart: "inspect_shadowy_room",
               text: "Shadowy room",
           }
       }
   },
-  option3a: {
+  read_contents: {
       text: `You read the book. You are possessed by an entity. You consume your twin.`,
       image: "assets/cover_house.jpg",
       options: {
@@ -83,21 +83,17 @@ const storyParts = {
           }
       }
   },
-  option3b: {
+  examine_binding: {
       text: `You examine the binding while Alex checks our the rest of the library. You are suddenly kidnapped and pass out.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option6a",
+              nextPart: "library_kidnap",
               text: "kidnapped!",
-          },
-          B: {
-            nextPart: "option6a",
-            text: "kidnapped!",
           }
       }
   },
-  option4a: {
+  inspect_basement: {
       text: `You enter the basement. TODO: Finish this path.  `,
       image: "assets/cover_house.jpg",
       options: {
@@ -111,8 +107,22 @@ const storyParts = {
           }
       }
   },
-  option4b: {
-      text: `You enter the shadowy room. TODO: Finish this path.  `,
+  inspect_shadowy_room: {
+      text: `You enter the shadowy room. You are accosted by a shadowy creature! run or fight?`,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "run_from_shadowy",
+              text: "run",
+          },
+          B: {
+              nextPart: "fight_shadowy",
+              text: "fight",
+          }
+      }
+  },
+  option5a: {
+      text: `TODO: Finish this path.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -124,6 +134,20 @@ const storyParts = {
               text: "play again",
           }
       }
-  }
+  },
+  option5b: {
+      text: `TODO: Finish this path.  `,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "start",
+              text: "play again",
+          },
+          B: {
+              nextPart: "start",
+              text: "play again",
+          }
+      }
+  },
   // ... continue structuring for all the nodes.
 };

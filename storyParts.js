a183c7d52e4501562d6e18a3bd2a8be4768e9298
Mayank Daswani <mdaswani@google.com>
2023-10-28 20:47:48 +0100

    Add text to flesh out story.

--- storyParts.js
+++ storyParts.js
@@ -1,6 +1,15 @@
 const storyParts = {
   start: {
-      text: `The twins arrive at the mansion. Alex impulsively rings the doorbell. The door opens.`,
+      text: `You're pedalling your bike, feeling the cool wind against your face. Up ahead, the old mansion stands tall and foreboding. Its darkened windows seem to stare back at you, and the twisted trees surrounding it add to its eerie charm. Every brick and shingle tells a story of age and mystery. Beside you, Alex, always the daredevil, throws you a challenging glance.
+
+      "Bet you wouldn't ring the doorbell," he teases.
+
+      "Are you serious?" you snap, unable to pull your gaze from the mansion.
+
+      Without waiting for your response, Alex speeds up, his bike coming to a halt right in front of the mansion's entrance. He hops off, marches up the steps, and impulsively presses the doorbell. The chime echoes eerily, and after a moment, the mansion's door creaks open to reveal a dimly lit hallway. Alex, curiosity getting the better of him, takes a step inside. You're left at the gate, heart racing.
+      
+      Do you follow Alex into the mysterious mansion, or do you turn around and head home, leaving him to his adventure?
+      `,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -14,7 +23,18 @@ const storyParts = {
       }
   },
   enter_together: {
-      text: `You enter the mansion together and look around. Library or hum?.`,
+      text: `As you step through the grand entrance, the first thing you notice is the dim lighting. The mansion's interior is illuminated by ornate chandeliers, their candles casting flickering shadows on the walls. The air is thick with a mix of dust and age, giving the place a feeling that time has forgotten it.
+
+      The main hall is expansive, with a grand staircase leading to the upper floors. On either side, there are doors leading to various rooms, their dark wooden frames intricately carved with designs that seem to move if you stare too long.
+      
+      The floor, made of polished marble, reflects the candlelight, and the walls are adorned with faded portraits of people who must've lived here ages ago. Their stern expressions make you uneasy, and you can't shake off the feeling that they're watching you.
+      
+      As you're taking in the surroundings, a soft humming sound reaches your ears. It's distant, almost melodic, and seems to be coming from a corridor to your left. There's something both enticing and unnerving about it.
+      
+      To your right, there's a slightly ajar door revealing a glimpse of a vast library. Rows upon rows of shelves filled with ancient books, their spines worn out from time. A soft glow emanates from the room, likely from a fireplace, casting a warm, inviting light.
+      
+      Torn between the two, you hesitate. Do you follow the mysterious humming, drawn to its haunting tune? Or do you seek refuge in the library, hoping to find answers among the old tomes?
+      `,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -28,7 +48,7 @@ const storyParts = {
       },
   },
   go_home: {
-      text: `You decide to go home, but Alex runs into the mansion. You follow him.`,
+      text: `Deciding this is way too spooky, you think it's best to turn around and go home. But as you do, you notice Alex has already ventured into the house, the door slowly closing behind him. Rolling your eyes, you realise you can't leave him alone in there. With a sigh, you follow him into the mansion.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -42,7 +62,20 @@ const storyParts = {
       },
   },
   enter_library: {
-      text: `You find yourself in an enormous library. Read book or examine binding?`,
+      text: `Choosing the safer option, you say, "Let's check out the library. Maybe we can find some answers there."
+
+      You and Alex head towards the door and step into the vast library. Rows upon rows of ancient books line the shelves, their spines worn and faded from time. The room is warmly lit by a fireplace, casting dancing shadows on the walls. But what immediately catches your attention is a single book displayed on a pedestal in the centre of the room.
+      
+      Approaching it, you see that the book is ancient, its pages yellowed and fragile. It's open to a particular page, and a passage is highlighted with a strange, glowing ink. The book's binding, however, is even more intriguing. It's made of an unfamiliar material, dark and leathery, with an odd texture that seems almost... alive.
+      
+      Alex leans in closer, squinting at the highlighted passage. "This looks important. Should we read it?"
+      
+      You hesitate, torn between the mysterious passage and the book's peculiar binding. "I don't know... Something about that binding gives me the creeps."
+      
+      Alex nods in agreement. "Yeah, it's definitely weird. But that passage... it might tell us something about this place."
+      
+      Now faced with another decision, do you choose to read the glowing passage out loud, hoping it provides answers? Or do you investigate the strange material of the book's binding, looking for clues about its origins?
+      `,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -56,12 +89,53 @@ const storyParts = {
       }
   },
   follow_hum: {
-      text: `You follow the hum towards a basement. You also notice a shadow in a nearby room. Basement or shadow?`,
+      text: `As you stand there, taking in the sights and sounds, Alex nudges you, pointing towards the corridor from where the humming is coming. "Hey, you hear that?" he whispers, his voice filled with a mix of excitement and apprehension.
+
+      "Yeah," you reply, listening intently. "It's... strange, isn't it?"
+      
+      Alex nods. "Wanna check it out? Could be a clue about this place."
+      
+      You hesitate for a moment, glancing towards the inviting library. "I don't know... The library seems safer."
+      
+      Alex grins, that adventurous spark in his eyes. "Come on, Susie. Where's your sense of adventure? Besides, it might just be a radio or something."
+      
+      You roll your eyes but can't help but smile. "Alright, alright. Let's follow the humming. But if we get into trouble, I'm blaming you."
+      
+      Alex chuckles. "Deal."
+      
+      With that, the two of you cautiously make your way towards the source of the humming, the sound growing louder with each step.
+      `,
       image: "assets/cover_house.jpg",
       options: {
           A: {
+              nextPart: "hum_or_shadow",
+              text: "Keep going...",
+          },
+      }
+  },
+  hum_or_shadow: {
+    text: `
+    As you and Alex continue down the corridor, the humming becomes more pronounced, echoing through the cold, draughty halls. But just as you're getting lost in its hypnotic rhythm, a swift movement catches your eye. A shadow, fleeting and elusive, darts into a room just off the corridor.
+      
+      Alex, noticing your distraction, follows your gaze. "Did you see that?" he asks, a hint of trepidation in his voice.
+      
+      You nod, heart racing. "Yeah. A shadow... or something. It went into that room."
+      
+      He glances at the door, then back at you, torn. "What do you think? Should we check it out, or stick to the humming?"
+      
+      You pause, weighing the options. On one hand, the shadow could be a clue, or perhaps someone who can help. On the other, the humming has an almost magnetic pull, beckoning you forward.
+      
+      "It might be nothing," you muse, "but what if it's important? Then again, the humming..."
+      
+      "Your call, Susie."
+      
+      Now faced with a decision, do you choose to investigate the mysterious shadow, or do you continue on, drawn by the humming?
+    `,
+    image: "assets/cover_house.jpg",
+      options: {
+          A: {
               nextPart: "inspect_basement",
-              text: "Basement",
+              text: "Continue on...",
           },
           B: {
               nextPart: "inspect_shadowy_room",
@@ -104,7 +178,24 @@ const storyParts = {
       }
   },
   inspect_shadowy_room: {
-      text: `You enter the shadowy room. You are accosted by a shadowy creature! run or fight?`,
+      text: `Taking a deep breath, you decide, "Let's check out that shadow. It could be a clue."
+
+      Alex nods, and together, you both cautiously approach the door and step into the room. The atmosphere inside is heavy, and a chill runs down your spine. The room appears to be an old study, with a large oak desk in the center and bookshelves lining the walls. But what captures your attention is the shadow on the floor.
+      
+      There's no discernible source for it. No person, no object casting it. Yet, there it is, dark and ominous. Before you can process this, the shadow starts to grow, stretching and distorting. It looms larger, taking on a more menacing form as it advances toward you.
+      
+      Alex's eyes widen in alarm. "What the...?"
+      
+      You take a step back, heart pounding in your chest.
+      
+      He clenches his fists, looking equally frightened and determined. "I don't even know how to fight a shadow, but if we run, where do we go?"
+      
+      The shadow continues its relentless approach, the room growing colder.
+      
+      "It's now or never," you say, trying to muster courage. "Stay and confront this... thing, or make a break for it."
+      
+      The choice is clear: Do you stand your ground against the shadow, or do you trust your instincts and run for safety?
+      `,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -116,6 +207,46 @@ const storyParts = {
               text: "fight",
           },
       }
+  },
+  fight_shadowy: {
+    text: `With a burst of courage, you shout, "We fight!" and brace yourself.
+
+    The shadow surges forward, its formless shape swirling and twisting. You try to fend it off, swinging and kicking at it, but it's like battling smoke. The more you move, the more it seems to envelop you.
+    
+    Suddenly, you feel a coldness seep into you, starting from your feet and rising. You glance down in horror to see your own shadow merging with the dark entity. It's consuming you, pulling you in. The coldness spreads, numbing your senses.
+    
+    "Alex!" you cry out, reaching for your brother. But your voice is distant, echoing as if you're at the bottom of a deep well.
+    
+    The world fades to black, the cold deepening, and you feel yourself sinking, the weight of the darkness pressing down.
+    
+    Then, nothingness.
+    `,
+    image: "assets/cover_house.jpg",
+    options: {
+        A: {
+            nextPart: "start",
+            text: "THE END."
+        }
+    }
+  },
+  run_from_shadowy: {
+    text: `Heart pounding, you shout, "Run!" and bolt towards the door, with Alex right behind you.
+
+    As you dash down the corridor, the haunting hum now drowned by your own frantic heartbeat, a chilling scream pierces the air. Glancing over your shoulder, you see the shadow enveloping Alex. It's pulling him in, his form merging with the darkness.
+    
+    "No!" you cry, reaching out for him. But the distance between you grows, and in a moment, Alex is gone, consumed entirely. The shadow, now larger and even more menacing, continues its relentless pursuit.
+    
+    Desperation fuels your steps, but it's in vain. The shadow catches up, and you feel the same coldness that took Alex start to seep into you. It begins at your feet, climbing, consuming, until darkness takes over.
+    
+    The corridor, the humming, the mansion - all fade away, leaving behind only the echoing memory of your brother's scream.
+    `,
+    image: "assets/cover_house.jpg",
+    options: {
+        A: {
+            nextPart: "start",
+            text: "THE END."
+        }
+    }
   },
   creep_in_dark: {
       text: `You creep forward in the dark. The basement is lit only by the sinister glow of the bottles. You trip - you knock over a bottle! the humming is broken, you are discovered!`,

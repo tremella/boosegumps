60c33184db9c2d4bc84bd7707d5377b289fe92cf
tremella <jeshbrand@gmail.com>
2023-10-28 19:22:01 +0100

    filling out story hash

--- storyParts.js
+++ storyParts.js
@@ -14,7 +14,7 @@ const storyParts = {
       }
   },
   option1a: {
-      text: `Result of Choice A. You enter the mansion together.`,
+      text: `You enter the mansion together and look around. Library or hum?.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -23,12 +23,12 @@ const storyParts = {
           },
           B: {
               nextPart: "option2b",
-              text: "Follow the mysterious sound",
+              text: "Follow the mysterious hum",
           }
       },
   },
   option1b: {
-      text: `Susie decides they should go home, it's too spooky! But when she turns around, she notices Alex has gone into the house. She rolls her eyes and follows him into the house.`,
+      text: `You decide to go home, but Alex runs into the mansion. You follow him.`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
@@ -42,18 +42,88 @@ const storyParts = {
       },
   },
   option2a: {
-      text: `Wow, it's a library. NERD SHIT.`,
+      text: `You find yourself in an enormous library. Read book or examine binding?`,
       image: "assets/cover_house.jpg",
       options: {
           A: {
-              nextPart: "option1",
-              text: "Enter mansion",
+              nextPart: "option3a",
+              text: "Read contents",
           },
           B: {
-              nextPart: "option2",
-              text: "Run away",
+              nextPart: "option3b",
+              text: "Examine binding",
           }
       }
   },
+  option2b: {
+      text: `You follow the hum towards a basement. You also notice a shadow in a nearby room. Basement or shadow?`,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "option4a",
+              text: "Basement",
+          },
+          B: {
+              nextPart: "option4b",
+              text: "Shadowy room",
+          }
+      }
+  },
+  option3a: {
+      text: `You read the book. You are possessed by an entity. You consume your twin.`,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "start",
+              text: "play again",
+          },
+          B: {
+              nextPart: "start",
+              text: "play again",
+          }
+      }
+  },
+  option3b: {
+      text: `You examine the binding while Alex checks our the rest of the library. You are suddenly kidnapped and pass out.`,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "option6a",
+              text: "kidnapped!",
+          },
+          B: {
+            nextPart: "option6a",
+            text: "kidnapped!",
+          }
+      }
+  },
+  option4a: {
+      text: `You enter the basement. TODO: Finish this path.  `,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "start",
+              text: "play again",
+          },
+          B: {
+              nextPart: "start",
+              text: "play again",
+          }
+      }
+  },
+  option4b: {
+      text: `You enter the shadowy room. TODO: Finish this path.  `,
+      image: "assets/cover_house.jpg",
+      options: {
+          A: {
+              nextPart: "start",
+              text: "play again",
+          },
+          B: {
+              nextPart: "start",
+              text: "play again",
+          }
+      }
+  }
   // ... continue structuring for all the nodes.
 };
